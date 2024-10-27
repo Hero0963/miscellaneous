772a329acde954eb61e10b6db1e793c8c5cc2550
ated0963 <ated0963@gmail.com>
2024-10-27 15:48:20 +0800

    feat(custom01_workflow): add initial implementation of custom01 workflow

---
+++ english_learning_tools/workflows/custom01_flow.py
@@ -0,0 +1,67 @@
+"""
+custom01_flow.py
+
+audio_file + source_txt
+
+"""
+
+import os
+import time
+import logging
+from abc import ABC
+
+from .workflow_base import WorkflowBase
+from ..transcriber.local_whisper_transcriber import transcribe_audio as chosen_transcriber
+from ..elt_utils.helper import transcribe_and_save
+from ..evaluation.text_evaluation import evaluate_transcription_naive
+
+
+class Custom01Flow(WorkflowBase, ABC):
+    def __init__(self, audio_file_path: str, source_txt_file: str):
+        super().__init__()
+
+        self.audio_file_path = audio_file_path
+        self.source_txt_file = source_txt_file
+        self.transcription_file = None
+
+    def generate_transcription_pair(self) -> 'Custom01Flow':
+        time_stamp = time.strftime("%Y%m%d_%H%M%S")
+        output_base = "D:\it_project\github_sync\Miscellaneous\english_learning_tools\processed_data"
+        transcription_file = os.path.join(output_base, f"result_{time_stamp}.txt")
+
+        transcribe_and_save(self.audio_file_path, transcription_file, chosen_transcriber)
+
+        self.transcription_file = transcription_file
+
+        return self
+
+    def evaluate(self) -> 'Custom01Flow':
+        logging.debug("Starting evaluation...")
+
+        accuracy_rate, redundancy_rate = evaluate_transcription_naive(self.source_txt_file, self.transcription_file)
+
+        logging.info(f'Accuracy Rate: {accuracy_rate * 100:.2f}%')
+        logging.info(f'Redundancy Rate: {redundancy_rate:.2f}%')
+
+        return self
+
+    def execute(self) -> 'Custom01Flow':
+        return self
+
+    def run(self) -> 'Custom01Flow':
+        return self.generate_transcription_pair().evaluate()
+
+
+def _simple_example():
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+
+    audio_file_path = r"D:\it_project\github_sync\Miscellaneous\english_learning_tools\validation_data\testing_sample.wav"
+    source_txt_file = r"D:\it_project\github_sync\Miscellaneous\english_learning_tools\validation_data\testing_sample.txt"
+    workflow = Custom01Flow(audio_file_path, source_txt_file)
+
+    workflow.run()
+
+
+if __name__ == "__main__":
+    _simple_example()
